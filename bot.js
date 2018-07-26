1849dcd2e95fa531a0c4f529c9a00be016206c3f
kinog502 <41550723+kinog502@users.noreply.github.com>
2018-07-26 21:33:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1503,24 +1503,6 @@ msg.channel.send(embed).then(() => {
 });
 
 
-client.on('ready', function(){	
-    var ms = 40000 ;	
-    var setGame = ['-inv','-help'];	
-    var i = -1;	
-    var j = 0;	
-    setInterval(function (){	
-        if( i == -1 ){	
-j = 1;	
-       }	
-        if( i == (setGame.length)-1 ){	
-            j = -1;	
-      }	
-       i = i+j;	
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);	
-}, ms);	
-	
-});
-
 
 client.on('message', message => {
   var prefix ="-"; 
