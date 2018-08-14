a0b06a4e65475a570375de4b1ee501dcf4f2edb0
kinog502 <41550723+kinog502@users.noreply.github.com>
2018-08-14 12:39:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2520,36 +2520,12 @@ client.on('message', function(message) {
     message.channel.send(`✅ | Done | Check Your DirectMessages <@${message.author.id}>`)
     message.author.send({embed})
   } 
+	 
 });
+
+
+
   
-
-client.on("ready", async  => {
-setInterval(function(){
-client.channels.find('id', '472352141650952192').setName("W");
-client.channels.find('id', '472352141650952192').setName("We");
-client.channels.find('id', '472352141650952192').setName("Wel");
-client.channels.find('id', '472352141650952192').setName("Welc");
-client.channels.find('id', '472352141650952192').setName("Welco");
-client.channels.find('id', '472352141650952192').setName("Welcom");
-client.channels.find('id', '472352141650952192').setName("Welcome");
-client.channels.find('id', '472352141650952192').setName("Welcome T");
-client.channels.find('id', '472352141650952192').setName("Welcome To");
-client.channels.find('id', '472352141650952192').setName("Welcome To h");
-client.channels.find('id', '472352141650952192').setName("Welcome To ho");
-client.channels.find('id', '472352141650952192').setName("Welcome To hos");
-client.channels.find('id', '472352141650952192').setName("Welcome To host");
-client.channels.find('id', '472352141650952192').setName("Welcome To host ");
-client.channels.find('id', '472352141650952192').setName("Welcome To host p");
-client.channels.find('id', '472352141650952192').setName("Welcome To host pl");
-client.channels.find('id', '472352141650952192').setName("Welcome To host plu");
-client.channels.find('id', '472352141650952192').setName("Welcome To host plus");
-
-
-  }, 30000);
-});
-
-
-
 client.on("ready", async  => {
 setInterval(function(){
 client.channels.find('id', '469692023515906061').setName("R");
@@ -2566,6 +2542,116 @@ client.channels.find('id', '469692023515906061').setName("Road To 2k");
 
   }, 3000);
 });
+
+
+
+
+
+
+
+
+client.on('guildMemberAdd', member => {
+     const welcomer =  member.guild.channels.find('name', 'welcome');
+    if(!welcomer) return;
+      if(welcomer) {
+         moment.locale('ar-ly');
+         var m = member.user;
+        let yumz = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(m.avatarURL)
+        .setAuthor(m.username,m.avatarURL)
+        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
+      
+         .setFooter(`${m.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
+     welcomer.send({embed:yumz});          
+         
+    
+
+
+
+const w = ['./img/w1.png'];
+
+         let Image = Canvas.Image,
+            canvas = new Canvas(400, 200),
+            ctx = canvas.getContext('2d');
+        fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+            if (err) return console.log(err);
+            let BG = Canvas.Image;
+            let ground = new Image;
+            ground.src = Background;
+            ctx.drawImage(ground, 0, 0, 400, 200);
+             
+          
+
+                let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(100) + ".png" : member.user.displayAvatarURL;
+                jimp.read(url, (err, ava) => {
+                    if (err) return console.log(err);
+                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                        if (err) return console.log(err);
+                        
+                        ctx.font = "bold 12px Arial";
+                        ctx.fontSize = '20px';
+                        ctx.fillStyle = "#f1f1f1";
+                        ctx.textAlign = "center";
+                        ctx.fillText(`welcome to ${member.guild.name}`, 300, 130);
+                        
+                        ctx.font = "bold 12px Arial";
+                        ctx.fontSize = '20px';
+                        ctx.fillStyle = "#f1f1f1";
+                        ctx.textAlign = "center";
+                        ctx.fillText(member.user.username, 200, 150);
+ 
+                let Avatar = Canvas.Image;
+                              let ava = new Avatar;
+                              ava.src = buf;
+                              ctx.beginPath();
+                              ctx.arc(77, 101, 62, 0, Math.PI*2);
+                              ctx.stroke();
+                                 ctx.clip();
+                                 ctx.drawImage(ava, 13, 38, 128, 126);  
+                          
+                
+                             
+welcomer.sendFile(canvas.toBuffer())
+
+
+
+      
+      
+                    }  )  
+      
+                    
+
+})
+      });                    
+ }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
