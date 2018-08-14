32eec8dcf096a48cb8b0b336cd60f79ed267b680
kinog502 <41550723+kinog502@users.noreply.github.com>
2018-08-14 13:03:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2548,29 +2548,10 @@ client.channels.find('id', '469692023515906061').setName("Road To 2k");
 
 
 
-
-
 client.on('guildMemberAdd', member => {
      const welcomer =  member.guild.channels.find('name', 'welcome');
-    if(!welcomer) return;
-      if(welcomer) {
-         moment.locale('ar-ly');
-         var m = member.user;
-        let yumz = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(m.avatarURL)
-        .setAuthor(m.username,m.avatarURL)
-        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
-      
-         .setFooter(`${m.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
-     welcomer.send({embed:yumz});          
-         
-    
-
-
-
-const w = ['./img/w1.png'];
-
+const w = ['./w1.png'];
+ 
          let Image = Canvas.Image,
             canvas = new Canvas(400, 200),
             ctx = canvas.getContext('2d');
@@ -2581,20 +2562,20 @@ const w = ['./img/w1.png'];
             ground.src = Background;
             ctx.drawImage(ground, 0, 0, 400, 200);
              
-          
-
+         
+ 
                 let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(100) + ".png" : member.user.displayAvatarURL;
                 jimp.read(url, (err, ava) => {
                     if (err) return console.log(err);
                     ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
                         if (err) return console.log(err);
-                        
+                       
                         ctx.font = "bold 12px Arial";
                         ctx.fontSize = '20px';
                         ctx.fillStyle = "#f1f1f1";
                         ctx.textAlign = "center";
-                        ctx.fillText(`welcome to ${member.guild.name}`, 300, 130);
-                        
+                        ctx.fillText(`welcome to Brix`, 300, 130);
+                       
                         ctx.font = "bold 12px Arial";
                         ctx.fontSize = '20px';
                         ctx.fillStyle = "#f1f1f1";
@@ -2609,155 +2590,118 @@ const w = ['./img/w1.png'];
                               ctx.stroke();
                                  ctx.clip();
                                  ctx.drawImage(ava, 13, 38, 128, 126);  
-                          
-                
+                         
+               
                              
 welcomer.sendFile(canvas.toBuffer())
-
-
-
-      
-      
+ 
+ 
+ 
+     
+     
                     }  )  
-      
-                    
-
+     
+                   
+ 
 })
       });                    
- }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  client.on("ready", () => {
-
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) });
+}
+client.on("ready", () => {
     var guild;
-
     while (!guild)
-
-        guild = client.guilds.get("460068204740345856");
-
+        guild = client.guilds.get("460068204740345856 ");
     guild.fetchInvites().then((data) => {
-
         data.forEach((Invite, key, map) => {
-
             var Inv = Invite.code;
-
             dat[Inv] = Invite.uses;
-
         });
-
     });
-
 });
-
  
-
  
-
  
-
 client.on("guildMemberAdd", (member) => {
-
     let channel = member.guild.channels.get("461145427237142528");
-
     if (!channel) {
-
         console.log("!the channel id it's not correct");
-
         return;
-
     }
-
     if (member.id == client.user.id) {
-
         return;
-
     }
-
     console.log('-');
-
     var guild;
-
     while (!guild)
-
         guild = client.guilds.get("460068204740345856");
-
     guild.fetchInvites().then((data) => {
-
         data.forEach((Invite, key, map) => {
-
             var Inv = Invite.code;
-
             if (dat[Inv])
-
                 if (dat[Inv] < Invite.uses) {
-
- channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;       
-
+                    setTimeout(function() {
+ channel.send(`**invited by** ${Invite.inviter} `) ;
+                    },1500);
  }
-
             dat[Inv] = Invite.uses;
-
        
-
        });
-
     });
-
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+ 
 
 
 
